48e153df3d1b7255d90ce03135fcc273d59fb931
providingperspectives <40553423+providingperspectives@users.noreply.github.com>
2024-07-02 18:37:35 +0100

    Update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,16 +6,16 @@ export const Footer = () => {
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <Link to="/" className="hover:underline">Cinema-hub</Link>. All Rights Reserved.</span>
           <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
               <li>
-                  <a href="https://www.facebook.com/julius.rowland" target="_blank" className="mr-4 hover:underline md:mr-6 ">Facebook</a>
+                  <a href="https://www.facebook.com/julius.rowland" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6 ">Facebook</a>
               </li>
               <li>
-                  <a href="https://www.linkedin.com/in/isiakpona-chuks-14b93235/" target="_blank" className="mr-4 hover:underline md:mr-6">LinkedIn</a>
+                  <a href="https://www.linkedin.com/in/isiakpona-chuks-14b93235/" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">LinkedIn</a>
               </li>
               <li>
-                  <a href="https://www.youtube.com/@TheIsiakpona" target="_blank" className="mr-4 hover:underline md:mr-6">Youtube</a>
+                  <a href="https://www.youtube.com/@TheIsiakpona" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">Youtube</a>
               </li>
               <li>
-                  <a href="https://github.com/providingperspectives" target="_blank" className="hover:underline">Github</a>
+                  <a href="https://github.com/providingperspectives" target="_blank" rel="noreferrer" className="hover:underline">Github</a>
               </li>
           </ul>
       </footer>
